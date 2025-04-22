d029d8f30372674012cd8d282b935b65de983a3a
chvsyam <chv.syamkumar@gmail.com>
2025-04-22 12:51:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ var http = require('http');
 //create a server object:
 http.createServer(function (req, res) {
   res.write('its starting'); //write a response to the client
-  res.write('its working'); //write a response to the client
+  res.write('/n its working'); //write a response to the client
   res.end(); //end the response
 }).listen(3000); //the server object listens on port 3000
